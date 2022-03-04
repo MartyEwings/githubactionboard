8d676165aade0a8c116cdb563b64bbc52cebf574
martyewings <martyewings@gmail.com>
2022-03-04 13:03:38 +0000

    Initial Commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,18 @@
-# @summary A short summary of the purpose of this class
+# @summary This module sets up the container provided by  https://github.com/otto-de/gitactionboard  
 #
-# A description of what this class does
+# Sets up a dashboard to visualise the results of github actions on a selected set of repos
 #
 # @example
 #   include githubactionboard
+# @param [String] githubaccesstoken
+#  Required, access token for registered github account, to precent rate limiting
+# @param [String] repoownername
+#  The repo namespace for which the monitored repositarys reside defaults to puppetlabs
+# @param [String] port
+#  The port the host machine will listen on and forward to the container
+# @param [Array[String]] reponames list of repos to monitor
 class githubactionboard (
-  Sensitive[String] $githubaccesstoken,
+  String $githubaccesstoken,
   String $repoownername = 'puppetlabs',
   Integer $port = 4444,
   Array[String[1]] $reponames = [influxdb,puppetlabs-pe_status_check,puppet_metrics_dashboard,puppetlabs-puppet_metrics_collector],
